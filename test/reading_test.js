3ff34ee8dad928452969c71bcdedd620373ba0c4
Roy Scheffers <r.scheffers@gmail.com>
2017-02-11 20:11:36 -0500

    automate test suite and finish .find and .findOne search tests

--- test/reading_test.js
+++ test/reading_test.js
@@ -3,18 +3,30 @@ const User = require('../src/user');
 const mongoose = require('mongoose');
 
 describe('Reading users out of the database', () => {
+  let roy;
 
   beforeEach( (done) => {
-    const roy = new User( { name: 'Roy'});
+    roy = new User( { name: 'Roy'});
     roy.save()
       .then( () => done() );
   });
 
-  it('finds all users with a name of Roy', () => {
-
+  it('finds all users with a name of Roy', (done) => {
+    User.find({ name: 'Roy' })
+      .then( (users) => {
+        // console.log(users[0]._id);
+        // console.log(roy._id);
+        // NOTE: _id is wrapped in an object, use .toString() to convert it to a string
+        assert( users[0]._id.toString() === roy._id.toString() );
+        done();
+      });
   });
 
-  it('finds specific userID', () => {
-
+  it('finds specific userID', (done) => {
+    User.findOne({ _id: roy._id })
+      .then( (user) => {
+        assert( user.name === roy.name );
+        done();
+      });
   });
 });
