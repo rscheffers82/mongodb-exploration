14ac10b5d457a4ac9629570dd11c8cb805352740
Roy Scheffers <r.scheffers@gmail.com>
2017-02-12 20:52:07 -0500

    Add 2 ways to update the instance of a model

---
+++ test/update_test.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Updating records', () => {
+  let roy;
+
+  beforeEach( (done) => {
+    roy = new User({ name: 'Roy Scheffers'});
+    roy.save()
+      .then( () => done() );
+  });
+
+// helper function to make the same assertions for our various DB actions / operations
+function assertName(operation, done) {
+  operation
+    .then( () => User.find({}) )
+    .then( (users) => {
+      assert(users.length === 1);
+      assert(users[0].name === 'R.G.G. Scheffers');
+      done();
+    });
+}
+
+  it('instance type using set \'n save', (done) => {
+    // use this to update (several properties on) an instance and THEN call save once
+    roy.set('name', 'R.G.G. Scheffers');
+    assertName(roy.save(), done);
+  });
+
+  it('A model instance can update', (done) => {
+    // Data is ready to go and can be updated, touches the DB right away
+    assertName( roy.update({ name: 'R.G.G. Scheffers' }), done );
+  });
+
+  // it('', () => {
+  //
+  // });
+  //
+  // it('', () => {
+  //
+  // });
+  //
+  // it('', () => {
+  //
+  // });
+});
